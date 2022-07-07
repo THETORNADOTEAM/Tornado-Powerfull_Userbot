1cb8c32e6b5270e5b31b920ff06c9562c72455fb
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-07-07 10:06:54 -0700

    Create Tornado_root.js

---
+++ Tornado_root.js
@@ -0,0 +1,7 @@
+from userbot- pip install python .await not if
+   await pro TORNADO git clone ==github.com/TORNADO??
+                  pip insall --uploding.
+                  pip insatll pro if await int("tornado_root").use
+                  pip insatll requrments.txt
+                  github root clone ("LEGENDBOT")str("TORNADOBOT")INT(CHENGE.AWAIT)not. CLONE==cmd.
+ FROM userbot - python.3
